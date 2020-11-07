4c00f26d6b8779d04ac238b24c6590a198acad14
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-11-07 20:52:15 +0300

    hw-1

--- AskMe/app/views.py
+++ AskMe/app/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html', {})
+def list_ask(request):
+    return render(request, 'list_ask.html', {})
 
 def add_ask(request):
     return render(request, 'add_ask.html', {})
 
+def ask(request):
+    return render(request, 'ask.html', {})
+
+def list_ask_tag(request):
+    return render(request, 'list_ask_tag.html', {})
+
 def settings(request):
     return render(request, 'settings.html', {})
 
-def register(request):
-    return render(request, 'register.html', {})
-
 def log_in(request):
     return render(request, 'log_in.html', {})
+
+def register(request):
+    return render(request, 'register.html', {})
