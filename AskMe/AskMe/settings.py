326cc8bd8dc4b41d2516d846d2b3151a5062bff0
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-11-26 16:09:16 +0300

    db sqlite3

--- AskMe/AskMe/settings.py
+++ AskMe/AskMe/settings.py
@@ -75,21 +75,21 @@ WSGI_APPLICATION = 'AskMe.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # for postgres
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'localhost',
-        'NAME': 'askme_db',
-        'USER': 'lnkn13',
-        'PASSWORD': '251099',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': 'localhost',
+#         'NAME': 'askme_db',
+#         'USER': 'lnkn13',
+#         'PASSWORD': '251099',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
