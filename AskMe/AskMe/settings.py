6127870d6fcd6f287390e30bd62b8e272f73b8de
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-12-03 13:26:16 +0300

    settings

--- AskMe/AskMe/settings.py
+++ AskMe/AskMe/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
