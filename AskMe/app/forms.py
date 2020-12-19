3b997d135243aa2cca2e9eb4856c061ac5b82123
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-12-20 00:15:21 +0300

    reworked forms Ask and Answer

--- AskMe/app/forms.py
+++ AskMe/app/forms.py
@@ -11,19 +11,51 @@ class LoginForm(forms.Form):
 
 
 class AskForm(forms.ModelForm):
+    tags = forms.CharField(required=False)
+
     class Meta:
         model = Question
         fields = ['title', 'text']
 
+    def __init__(self, profile_id, **kwargs):
+        self._profile_id = profile_id
+        super(AskForm, self).__init__(**kwargs)
 
-class TagForm(forms.Form):
-    tags = forms.CharField(required=False)
+    def clean_tags(self):
+        self.tags = self.cleaned_data['tags'].split()
+        if len(self.tags) > 3:
+            self.add_error(None, 'use no more than 3 tags!')
+            raise forms.ValidationError('use no more than 3 tags!')
+        return self.tags
+
+    def save(self, **kwargs):
+        question = Question()
+        question.profile_id = self._profile_id
+        question.title = self.cleaned_data['title']
+        question.text = self.cleaned_data['text']
+        question.save()
+
+        for tag in self.tags:
+            if not Tag.objects.filter(tag=tag).exists():
+                Tag.objects.create(tag=tag)
+        question.tags.set(Tag.objects.filter(tag__in=self.tags))
+        return question
 
 
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields = ['text']
+
+    def __init__(self, profile_id, question_id, **kwargs):
+        self._profile_id = profile_id
+        self._question_id = question_id
+        super(AnswerForm, self).__init__(**kwargs)
+
+    def save(self, **kwargs):
+        self.cleaned_data['profile_id'] = self._profile_id
+        self.cleaned_data['question_id'] = self._question_id
+        return Answer.objects.create(**self.cleaned_data)
 
 
 class SignupForm(forms.ModelForm):
--- AskMe/app/views.py
+++ AskMe/app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from app.models import Question, Answer, Tag, Profile
 from django.contrib.auth.models import User
-from app.forms import LoginForm, AskForm, TagForm, AnswerForm, SignupForm, SettingsForm
+from app.forms import LoginForm, AskForm, AnswerForm, SignupForm, SettingsForm
 
 
 def paginate(objects_list, request, limit=2):
@@ -26,40 +26,25 @@ def new_questions(request):
 @login_required
 def create_ask(request):
     if request.method == 'POST':
-        ask_form = AskForm(data=request.POST)
-        tag_form = TagForm(data=request.POST)
-        if ask_form.is_valid() and tag_form.is_valid():
-            question = ask_form.save(commit=False)
-            question.profile_id = request.user.profile
-            question.save()
-
-            tags = tag_form.cleaned_data.get('tags').split()
-            if len(tags) > 3 or len(tags) == 0:
-                ask_form.add_error(None, 'Max 3 tags!')
-            else:
-                for _tag in tags:
-                    if not Tag.objects.filter(tag=_tag).exists():
-                        Tag.objects.create(tag=_tag)
-                question.tags.set(Tag.objects.filter(tag__in=tags))
-
+        ask_form = AskForm(request.user.profile, data=request.POST)
+        if ask_form.is_valid():
+            question = ask_form.save()
             return redirect(reverse('question', kwargs={'pk': question.pk}))
     return render(request, 'create_ask.html', {})
 
 
 def question_page(request, pk):
     question = Question.objects.get(id=pk)
-    answers_page = paginate(Answer.objects.by_question(pk), request, 1)
+    answers_page = paginate(Answer.objects.by_question(pk), request, limit=1)
+
     if request.method == 'POST':
         if not request.user.is_authenticated:
             return redirect(f"/login/?next={request.get_full_path()}")
 
-        form = AnswerForm(data=request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.profile_id = request.user.profile
-            answer.question_id = question
-            answer.save()
-            answers_page = paginate(Answer.objects.by_question(pk), request, 1)
+        answer_form = AnswerForm(request.user.profile, question, data=request.POST)
+        if answer_form.is_valid():
+            answer_form.save()
+            answers_page = paginate(Answer.objects.by_question(pk), request, limit=1)
             return redirect(reverse('question', kwargs={'pk': pk}) + f"?page={answers_page.paginator.num_pages}")
 
     return render(request, 'question.html', {
@@ -163,4 +148,6 @@ def signup(request):
                 Profile.objects.create(user_id=user, avatar=avatar)
                 login(request, user)
                 return redirect(request.POST.get('next', '/'))
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html', {
+        'form': form,
+    })
