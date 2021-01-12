a81b5d0d8f989ca6a2b7df4ae16f0214430fc0b3
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-01-13 02:44:16 +0300

    heroku 6

--- AskMe/manage.py
+++ AskMe/manage.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AskMe.settings")
 
-    from django.core.management import execute_from_command_line
-
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskMe.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+        ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
