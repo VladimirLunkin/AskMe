cb60fbc5b162d985c705be57547861c40abe048d
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-01-13 02:24:20 +0300

    heroku 1

--- AskMe/AskMe/settings.py
+++ AskMe/AskMe/settings.py
@@ -99,9 +99,9 @@ LOGIN_URL = '/login/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
