55508c49c2cab88a3bad1a2e39e9491893fab687
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-01-13 15:46:20 +0300

    heroku 31

--- AskMe/AskMe/wsgi.py
+++ AskMe/AskMe/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskMe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskMe.AskMe.settings')
 
 application = get_wsgi_application()
